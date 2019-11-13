3e93ef0f35226abe7780d382ba0e6b8276eabf2a
mperry37 <mperry37@alaska.edu>
2019-11-12 23:00:39 -0900

    passing all but one.

--- treesort.h
+++ treesort.h
@@ -1,7 +1,10 @@
 // treesort.h  SKELETON
-// Glenn G. Chappell
-// 2019-11-10
+// Matt Perry
+// Tailon Russell
+// Started: 11-10-2019
+// Finished:
 //
+// Template supplied by Glenn G. Chappell 
 // For CS 311 Fall 2019
 // Header for function template treesort
 // There is no associated source file.
@@ -20,8 +23,6 @@
 #include <memory>
 // For std::unique_ptr
 
-#include <iostream>
-
 template<typename ValType>
 struct Node 
 { 
@@ -29,25 +30,16 @@ struct Node
     std::unique_ptr<Node<ValType>> _left;
     std::unique_ptr<Node<ValType>> _right; 
 
-    explicit Node(const ValType & key): _key(key) //,
-                //std::unique_ptr<Node<ValType>> & left = nullptr,
-                //std::unique_ptr<Node<ValType>> & right = nullptr)
-        //:_key(key),
-        //_left(std::move(left)),
-       // _right(std::move(right))
+    explicit Node(const ValType & key): _key(key)
     {}
 
     ~Node() = default;
 }; 
 
 template<typename ValType>
-std::unique_ptr<Node<ValType>> & newNode(ValType key)
+std::unique_ptr<Node<ValType>> newNode(const ValType & key)
 {
-	//std::unique_ptr<Node<ValType>> newN(key);
 	auto newN = std::make_unique<Node<ValType>>(key);
-	//newN->_key = key;
-	//newN->_left = nullptr;
-	//newN->_right = nullptr;
 	return newN;
 }
 
@@ -65,48 +57,34 @@ void treesort(FDIter first, FDIter last)
 {
     // ValType is the type that FDIter points to
     using ValType = typename std::iterator_traits<FDIter>::value_type;
-    std::vector<ValType> range(std::distance(first, last));
     std::vector<ValType> endResult;
-    std::move(first, last, range.begin());
-    std::unique_ptr<Node<ValType>> root = nullptr;
-	if (range.size() == 0)
-	{
-		
-	}
-	else
-	{
-		std::move(insert(root, range[0]));
-		for (auto i = 1; i < range.size(); i++)
-		{
-			insert(root, range[i]);
-		}
-		traverse(root, endResult);
-		std::move(endResult.begin(), endResult.end(), first);
-	}
+    std::unique_ptr<Node<ValType>> root;
+    for (auto i = first; i != last; i++)
+    {
+        insert(root, *i);
+    }
+    traverse(root, endResult);
+    std::move(endResult.begin(), endResult.end(), first);
 }
 
 
 template<typename ValType>
- std::unique_ptr<Node<ValType>> & insert(std::unique_ptr<Node<ValType>> & node, ValType & key)
+ void insert(std::unique_ptr<Node<ValType>> & node, ValType & key)
 {
     if (node == nullptr)
     {
-		//node->_key = key;
-		//return node;
-		return newNode(key);
-    } 
-  
+        node = newNode(key);
+    }   
     /* Otherwise, recur down the tree */
-    if (key < node->_key) 
+    else if (key < node->_key) 
     {
-        node->_left = std::move(insert(node->_left, key)); 
+        insert(node->_left, key); 
     }
-    else 
+    else
     {
-        node->_right = std::move(insert(node->_right, key)); 
-  
+        insert(node->_right, key); 
     }
-    return node; 
+
 }
 
 template<typename ValType>
